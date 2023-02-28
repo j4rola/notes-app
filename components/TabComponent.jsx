96c60507aff7625e03260f169144a1abe4c1deb3
Joe Froula <jmfroula@gmail.com>
2023-02-27 19:50:30 -0800

    testing env

--- components/TabComponent.jsx
+++ components/TabComponent.jsx
@@ -1,23 +1,41 @@
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
-import {Tab, Tabs} from 'react-bootstrap/Tab';
+import Tab from 'react-bootstrap/Tab';
+
+  const getData = () => {
+    return null
+  }
 
 
-
-function TabComponent({tab}) {
-
-    const getData = () => {
-      return null
-    }
+function TabComponent() {
   return (
-    <div>
-        {tab.map( x => <div>
-            {x.Title}
-        </div>)}
-        
-    </div>
+    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+      <Row>
+        <Col sm={3}>
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item>
+              <Nav.Link>Tab 1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>Tab 2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+            <Tab.Pane >
+            <p>test 1</p>
+            </Tab.Pane>
+            <Tab.Pane >
+              <p>test 2</p> 
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
   );
 }
+
 
 export default TabComponent;
